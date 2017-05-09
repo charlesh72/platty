0886e976f0431d20ffcead4fd6fc0643bb460365
Charles <Charles.h7272@gmail.com>
2017-05-09 15:17:54 -0700

    set up project for working with google cloud messaging

---
+++ app/src/main/java/com/beakon/platty/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package com.beakon.platty;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Beakon on 5/9/2017.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public static final String TAG = "MyFireInstIDServ";
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+}
